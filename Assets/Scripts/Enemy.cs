efcd0a957b517101ed15cf77cc446ba8f02ab2af
EmptyRip <leha_alpatov@mail.ru>
2022-01-16 16:28:55 +0300

    add

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Vector3 _spawn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Transform>(out Transform player))
+        {
+            player.position = _spawn;
+        }
+    }
+}
